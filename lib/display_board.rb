c33f7041c7e56830105c04fe1994b9c6b4fe398d
Shane Kuziel <shane.kuziel@trojans.dsu.edu>
2018-06-27 18:20:36 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,11 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
   empty = puts " "
-  puts "#{empty} + | + #{empty}+ | + #{empty}"
+  puts "#{empty} | #{empty} | #{empty}"
   puts "-----------"
-  puts "#{empty} + | + #{empty}+ | + #{empty}"
+  puts "#{empty} | #{empty} | #{empty}"
   puts "-----------"
-  puts "#{empty} + | + #{empty}+ | + #{empty}"
+  puts "#{empty} | #{empty} | #{empty}"
 end
 
 return display_board
