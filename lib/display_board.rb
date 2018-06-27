066716339ff960b1e051dfbc6d3633fe0692b3d4
Shane Kuziel <shane.kuziel@trojans.dsu.edu>
2018-06-27 18:16:06 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,7 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+empty = " "
 def display_board
-  print (" | | ")
+  puts empty + "|" + empty + "|" + empty 
   print -----------
   print (" | | ")
   print -----------
